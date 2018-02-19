3fe65a1495823b2fbf5dccce12185d6d11bdf1ca
fx <fhannedouche@lacie.com>
2018-02-19 11:21:26 +0100

    [touchscreen] Demo app

--- UI/main.cpp
+++ UI/main.cpp
@@ -1,13 +1,29 @@
 
-
-#include "ucg.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "Ucglib.h"
 
 extern "C"
 {
 int ucg_sdl_get_key(void);
 }
+void Ucglib::init()
+{
+    
+}
+class  UcglibSdl : public Ucglib
+{
+  public:
+    UcglibSdl()
+      { 
+         ucg_Init( &ucg,&ucg_sdl_dev_cb, ucg_ext_none, (ucg_com_fnptr)0);
+      }
+    void begin(uint8_t is_transparent)
+    {
+        
+    }
+};
+
 
 //extern const ucg_fntpgm_uint8_t ucg_font_ncenB18[] UCG_FONT_SECTION("ucg_font_ncenB18");
 //extern const u8g_fntpgm_uint8_t u8g_font_ncenB18[] U8G_FONT_SECTION("u8g_font_ncenB18");
@@ -17,37 +33,26 @@ ucg_t ucg;
 
 int main(void)
 {
-  
-  ucg_Init(&ucg, &ucg_sdl_dev_cb, ucg_ext_none, (ucg_com_fnptr)0);
-  ucg_SetFontMode(&ucg, UCG_FONT_MODE_SOLID);
-  ucg_SetFont(&ucg, ucg_font_ncenB24_hf);
+  UcglibSdl ucg;
+  ucg.setFontMode( UCG_FONT_MODE_SOLID);
+  ucg.setFont( ucg_font_ncenB24_hf);
 
   
-  /*
-  ucg_SetColor(&ucg, 0, 255, 180, 40);
-  ucg_DrawGlyph(&ucg, 64, 32,0, 'A');
-  ucg_SetColor(&ucg, 0, 255, 200, 70);
-  ucg_DrawGlyph(&ucg, 64, 32,1, 'A');
-  ucg_SetColor(&ucg, 0, 255, 180, 40);
-  ucg_DrawGlyph(&ucg, 64, 32,2, 'A');
-  ucg_SetColor(&ucg, 0, 255, 200, 70);
-  ucg_DrawGlyph(&ucg, 64, 32,3, 'A');
-  */
-  
-  ucg_SetColor(&ucg, 1, 0, 0, 255);
-  ucg_SetColor(&ucg, 0, 255, 180, 40);
-  ucg_DrawString(&ucg, 64, 32,0, "AB");
-  ucg_SetColor(&ucg, 0, 255, 200, 70);
-  ucg_DrawString(&ucg, 64, 32,1, "AB");
-  ucg_SetColor(&ucg, 0, 255, 180, 40);
-  ucg_DrawString(&ucg, 64, 32,2, "AB");
-  ucg_SetColor(&ucg, 0, 255, 200, 70);
-  ucg_DrawString(&ucg, 64, 32,3, "AB");
 
-  ucg_SetColor(&ucg, 0, 128, 128, 255);
-  ucg_DrawPixel(&ucg, 64, 32);
+  ucg.setColor( 1, 0, 0, 255);
+  ucg.setColor( 0, 255, 180, 40);
+  ucg.drawString( 64, 32,0, "AB");
+  ucg.setColor( 0, 255, 200, 70);
+  ucg.drawString( 64, 32,1, "AB");
+  ucg.setColor( 0, 255, 180, 40);
+  ucg.drawString( 64, 32,2, "AB");
+  ucg.setColor( 0, 255, 200, 70);
+  ucg.drawString( 64, 32,3, "AB");
 
-  //ucg_DrawHLine(&ucg, 0,64,20);
+  ucg.setColor( 0, 128, 128, 255);
+  ucg.drawPixel( 64, 32);
+
+  //ucg.drawHLine( 0,64,20);
   
 
   while( ucg_sdl_get_key() < 0 )

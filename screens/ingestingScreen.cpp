25c76250f3785cc139509b5ba80b929a06ee65c2
fx <fhannedouche@lacie.com>
2018-02-22 11:59:54 +0100

    [touchy] dumb progress bar

--- screens/ingestingScreen.cpp
+++ screens/ingestingScreen.cpp
@@ -36,17 +36,20 @@ void ingestingScreen::draw(Ucglib *ucg)
     
     float f=ucg->getWidth()*_percent;
     f/=100;
+    
+    int third=(int)f;
+    
             
     int centery=(h*1)/3;
     ucg->setColor(0, 255, 255, 255);
     ucg->drawBox(0,centery-15,w-1,30);
     ucg->setColor(0, 0, 255, 0);
-    ucg->drawBox(1,centery-14,f-2,28);
+    ucg->drawBox(1,centery-14,third-2,28);
     
     char str[50];
     sprintf(str," %d %% done",_percent);
     ucg->setColor(0, 255, 255, 255);
-    ucg->drawString(10,f*2,0,str);
+    ucg->drawString(10,third*2,0,str);
    
  
 }

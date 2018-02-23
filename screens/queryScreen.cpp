6e0f588f3761b57d76cd7c7880a24e1601f5eb29
fx <fhannedouche@lacie.com>
2018-02-23 11:42:54 +0100

    [touchscreen] Add logo

--- screens/queryScreen.cpp
+++ screens/queryScreen.cpp
@@ -6,6 +6,8 @@
 #include "screen.h"
 #include "touchyDebug.h"
 #include "screenManager.h"
+#include "sdcard.h"
+#include "screenUtil.h"
 #define TYPE_SD 1
 #define TYPE_USB 2
 
@@ -25,34 +27,6 @@ public:
         virtual void draw(Ucglib *ucg);
         virtual bool touched(Ucglib *ucg,int x, int y);    
 };
-/**
- */
-void drawSdCard(Ucglib *ucg, int x, int y, int w)
-{
-  int sd_height = w*1.3;
-  int sd_bar_width = w/8;
-  int sd_bar_height = w/4.1;
-  int sd_bar_space = w/17.6;
-  if(0 == sd_bar_space){
-    sd_bar_space = 1;
-  }
-  int sd_triangle = w/3.3;
-  int sd_round = sd_bar_space;
-
-  // main shape
-  ucg->setColor(0, 94, 122, 231);
-  ucg->drawRBox(x, y, w, sd_height, sd_round);
-  ucg->setColor(0, 0, 0, 0);
-  ucg->drawTriangle(x, y, x + sd_triangle, y, x, y + sd_triangle);
-
-  // connectors
-  ucg->setColor(0, 255, 255, 255);
-  ucg->drawBox(x + sd_triangle - sd_bar_space - sd_bar_width, y + sd_triangle / 2 + (sd_bar_space * 2), sd_bar_width, sd_bar_height);
-  for (int i = 0; i < 4; i++)
-  {
-    ucg->drawBox(x + sd_triangle + i * (sd_bar_space + sd_bar_width), y + (sd_bar_space * 2), sd_bar_width, sd_bar_height);
-  }
-}
 /**
  */
 void drawButton(Ucglib *ucg, int x, int y, int type)
@@ -96,8 +70,8 @@ void drawAskIngest(Ucglib *ucg, int type)
   switch(type){
     case TYPE_SD:
       {
-        int sd_width = 40;
-        drawSdCard(ucg, ucg->getWidth() / 2 - sd_width / 2, 20, sd_width);
+            drawBitmap(ucg,160-80,0, (const uint8_t *)sdcard_logo,160,160,0,0xffff);
+       
       }
       break;
     case TYPE_USB:
@@ -112,7 +86,7 @@ void drawAskIngest(Ucglib *ucg, int type)
   ucg->setColor(0, 255, 255, 255);
 
   // TODO Center text!!!
-  ucg->drawString(60, 120, 0, "Copy SD card ?");
+  ucg->drawString(60, 160, 0, "Copy SD card ?");
   drawButton(ucg, 30 , ucg->getHeight() - 60, 2);
   drawButton(ucg, ucg->getWidth()-30-100, ucg->getHeight() - 60, 1); 
 }
--- screens/screenUtil.cpp
+++ screens/screenUtil.cpp
@@ -9,8 +9,8 @@
 /**
  */
 #include "Adafruit_ILI9341_STM.h"
-
-
+// slow mode
+#if 0
 void drawBitmap(Ucglib *ucg,int wx,int wy, const uint8_t *data, int width, int height, int fgcolor,int bgcolor)
 {
     uint8_t *p=(uint8_t *)data;
@@ -32,3 +32,35 @@ void drawBitmap(Ucglib *ucg,int wx,int wy, const uint8_t *data, int width, int h
         }        
     }   
 }
+#else
+// fast mode
+// Prepare bitmap and send it line per line
+void drawBitmap(Ucglib *ucg,int wx,int wy, const uint8_t *data, int width, int height, int fgcolor,int bgcolor)
+{
+    uint8_t *p=(uint8_t *)data;
+    Adafruit_ILI9341_STM *tft=ucg->getTft();
+    static uint16_t line[320];
+    
+    width>>=3;
+    for(int y=0;y<height;y++)
+    {
+        uint16_t *o=line;
+        tft-> setAddrWindow(wx, wy+y, wx+width*8, wy+y);
+        for(int x=0;x<width;x++)
+        {
+            int stack=*p++;
+            for(int step=0;step<8;step++)
+            {
+                int color;
+                if(stack&0x80)                                        
+                    color=fgcolor;
+                else
+                    color=bgcolor;
+                *o++=color;
+                stack<<=1;
+            }            
+        }    
+        tft->pushColors(line,width*8,0);
+    }   
+}
+#endif

59e21a5857094a6d360f05551b0105f125382a35
fx <fhannedouche@lacie.com>
2018-03-05 14:57:52 +0100

    [touchy] use bitmap for battery

--- screens/idleScreen.cpp
+++ screens/idleScreen.cpp
@@ -5,7 +5,11 @@
 #include "myLcd.h"
 #include "screen.h"
 #include "touchyDebug.h"
-
+#include "0bat.h"
+#include "1bat.h"  
+#include "2bat.h"
+#include "3bat.h"
+#include "screenUtil.h"
 /**
  */
 
@@ -23,6 +27,7 @@ public:
             free(_txt);
         }
         virtual void draw(Ucglib *ucg);
+        void        drawBattery(Ucglib *ucg);
         virtual void redraw(Ucglib *ucg,const char **arg);
         virtual bool touched(Ucglib *ucg,int x, int y);    
         int          _bat,_free;
@@ -31,78 +36,38 @@ public:
 /**
  */
 
-static void drawAroundBattery(Ucglib *ucg)
+static int bat2bar(int bat)
 {
-  int battery_width = 84;
-  int battery_height = 48;
-  int battery_tit_width = 8;
-  int battery_tit_height = 19;
-  int battery_pos_x = ucg->getWidth() / 2 - (battery_width + battery_tit_width) / 2;
-  int battery_pos_y = 30;
-  int battery_bar_width = 12;
-  int battery_bar_height = 36;
-  int battery_space_bar = 3;
-
-  ucg->drawRFrame(battery_pos_x, battery_pos_y, battery_width, battery_height, 4); // main shape
-  ucg->drawFrame(battery_pos_x + 1, battery_pos_y + 1, battery_width - 2, battery_height - 2);
-  ucg->drawFrame(battery_pos_x + 2, battery_pos_y + 2, battery_width - 4, battery_height - 4);
-  ucg->drawRBox(battery_pos_x + battery_width - 1, battery_pos_y + battery_height / 2 - battery_tit_height / 2,
-                battery_tit_width + 1, battery_tit_height, 2); // tit
-}
-static void drawSingleBar(Ucglib *ucg,int i)
-{
-  int battery_width = 84;
-  int battery_height = 48;
-  int battery_tit_width = 8;
-  int battery_tit_height = 19;
-  int battery_pos_x = ucg->getWidth() / 2 - (battery_width + battery_tit_width) / 2;
-  int battery_pos_y = 30;
-  int battery_bar_width = 12;
-  int battery_bar_height = 36;
-  int battery_space_bar = 3;
-    ucg->drawBox(battery_pos_x + 3 + battery_space_bar + battery_space_bar * i + battery_bar_width * i, battery_pos_y + battery_space_bar + 3,
-                 battery_bar_width, battery_bar_height); 
+    if(bat>90) return 3;
+    if(bat>55) return 2;
+    if(bat>20) return 1;
+    return 0;
 }
 
+int mkColor(int r,int g, int b)
+{
+    r=((0x1f*r)>>8);
+    g=((0x1f*r)>>8);
+    b=((0x1f*r)>>8);
+    return (r<<11)+(g<<5)+b;
+}
 
 /**
  */
-void drawBattery(Ucglib *ucg, int battery_level)
+void dummyScreen::drawBattery(Ucglib *ucg)
 {
-  int count=1;
-  
-  if (battery_level > 92)
-  {
-    ucg->setColor(0, 0, 199, 20);
-    count=5;
-  }
-  else if (battery_level > 72)
-  {
-    ucg->setColor(0, 139, 196, 65);
-    count=4;
-  }
-  else if (battery_level > 52)
-  {
-    ucg->setColor(0, 248, 172, 65);
-    count=3;
-  }
-  else if (battery_level > 32)
-  {
-    ucg->setColor(0, 237, 91, 39);
-    count=2;
-  }
-  else
-  {
-    ucg->setColor(0, 233, 29, 33);
-    count=1;
-  }
-  
-  drawAroundBattery(ucg);
-  for(int i=0;i<count;i++)
-      drawSingleBar(ucg,i);
-  ucg->setColor(0, 0,0, 20);    
-  for( int i=count;i<5;i++)
-      drawSingleBar(ucg,i);
+    const char *bitmap;
+    int fgcolor;
+    switch(_bat)
+    {
+        default: 
+        case 3:            bitmap=bat0; fgcolor=mkColor(233,29,33);break;
+        case 2:            bitmap=bat1; fgcolor=mkColor(233,29,33);break;
+        case 1:            bitmap=bat2; fgcolor=mkColor(233,29,33);break;
+        case 0:            bitmap=bat3; fgcolor=mkColor(233,29,33);break;
+        
+    }    
+    drawBitmap(ucg,80,10,(const uint8_t *)bitmap,160,106,fgcolor,0);    
  
 }
 /**
@@ -124,19 +89,24 @@ void dummyScreen::redraw(Ucglib *ucg,const char **arg)
 {
 
  LOG("DRAWING idle");
+ int redraw=false;
+ int oldbat=_bat;
  if(arg)
  {
      free(_txt);
      _txt=strdup(arg[2]);
-     _bat=atoi(arg[0]);
+     _bat=bat2bar(atoi(arg[0]));
      _free=atoi(arg[1]);
+    if(_bat != oldbat)
+        drawBattery(ucg);
+ }else
+ {
+    drawBattery(ucg);
  }
   /* 
    * Battery
    */
-
-  int battery_level = _bat;
-  drawBattery(ucg,battery_level);
+  
 
   ucg->setColor(0, 255, 255, 255); // withe color for the text
 
@@ -153,12 +123,10 @@ void dummyScreen::redraw(Ucglib *ucg,const char **arg)
   {
       sprintf(str,"  Free : %d GB  ",sz);
   }
-  ucg->getTft()-> fillRect(0,100,320,130,0);
+  ucg->getTft()-> fillRect(0,120,320,130,0);
   ucg->setColor(0, 255, 255, 255); // withe color for the text
-  centeredPrint(ucg,str,130);
-  centeredPrint(ucg,_txt,190);
-
-  
+  centeredPrint(ucg,str,150);
+  centeredPrint(ucg,_txt,210);
  
 }
 
@@ -172,7 +140,7 @@ bool dummyScreen::touched(Ucglib *ucg,int x, int y)
 
 Screen *dummySpawner(const char **args)
 {
-    int bat=atoi(args[0]);
+    int bat=bat2bar(atoi(args[0]));
     int fre=atoi(args[1]);
     char *txt=(char *)args[2];
     return new dummyScreen(bat,fre,txt);
